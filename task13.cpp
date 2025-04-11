36325fa05bbcc27bcdd1546a71c6ed82569347cb
umer <umery1730@gmail.com>
2025-04-11 17:51:24 +0500

    add

---
+++ task13.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int person1,person2;
+    int age;
+    cout<<"Person1 age:";
+    cin>>person1;
+    cout<<"Person2 age:";
+    cin>>person2;
+    if (age > 18)
+    {
+        cout<<"person1";
+    }
+    else
+    {
+        cout<<"person2";
+    }
+}
