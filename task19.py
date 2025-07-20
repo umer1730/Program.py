9aab29ea5fb55ee91f36da8ed3082b926c6ef113
umer <umery1730@gmail.com>
2025-07-21 00:05:45 +0500

    add

---
+++ task19.py
@@ -0,0 +1,9 @@
+lbs  = float(input("Enter: "))
+kgs = lbs / 2.205
+print(lbs," lbs has ",kgs," kgs")
+
+#next code
+length = int(input("Enter length:"))
+width = int(input("Enter width:"))
+area = length * width
+print("The area is:",area)
