e767549268831f6d3e8ac912b425cc5f5ea13dba
umer <umery1730@gmail.com>
2025-08-01 00:17:55 +0500

    add

---
+++ Pandas/describe.py
@@ -0,0 +1,13 @@
+# step1 sample data frame
+import pandas as pd
+data = {
+    "Name":["Saim","Ahmer","Ashar","Bilal","Hurais","Haris"],
+    "Age":[10,20,30,40,50,60],
+    "Salary":[100,200,300,400,500,600],
+    "Performance score":[10,20,30,40,50,60]
+}
+df = pd.DataFrame(data)
+print("Sample DataFrame")
+print(df)
+print("---Descriptive Statistics---")
+print(df.describe())
