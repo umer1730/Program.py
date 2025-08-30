1a8b448f74afa84de5cd37c7f531f81c1212c10d
umer <umery1730@gmail.com>
2025-08-30 08:42:09 +0500

    add

---
+++ Projects/to_do.py
@@ -0,0 +1,35 @@
+def task():
+    tasks=[]
+    print("----Welcome to the task management app----")
+    total_task=int(input("Enter how many task you want to add = " ))
+    for i in range(1, total_task+1):
+        task_name=input(f"Enter task{i} = ")
+        tasks.append(task_name)
+    print(f"Today's tasks are\n{task}")
+    while True:
+        operation=int(input("Enter 1-Add\n2-Update\n3-Delete\n4-View\n5-Exit/Stop"))
+        if operation == 1:
+            add = input("Enter task you want to add = ")
+            tasks.append(add)
+            print(f"Task {add} has been add successfully..")
+        elif operation == 2:
+            updated_val=input("Enter the task name you want to update = ")
+            if updated_val in tasks:
+                up = input("Enter new task = ")
+                ind = tasks.index(updated_val)
+                tasks[ind]=up
+                print(f"Updated task {up}")
+        elif operation == 3:
+            del_val = input("Which task you want to delete = ")
+            if del_val in tasks:
+                ind = tasks.index(del_val)
+                del tasks[ind]
+                print(f"Task {del_val} has been deleted successfully")
+        elif operation == 4:
+            print(f"Total tasks={tasks}")
+        elif operation == 5:
+            print("closing the program")
+            break
+        else:
+            print("Invalid input")
+task()
