1178a82e486ede7cf26e7516e8fdf7d66f3c6ead
umer <umery1730@gmail.com>
2025-09-05 13:37:40 +0500

    add

---
+++ Machine learning/task1.py
@@ -0,0 +1,13 @@
+question = [50,70,70,90,45,76,99,88]
+mean= 50+70+70+90+45+76+99+88 / 8
+print(mean)
+print("--------------")
+
+question = [50,70,70,90,45,76,99,88]
+meadian= 90+45 / 2
+print(meadian)
+print("--------------")
+
+question = [50,70,70,90,45,76,99,88]
+mode= 70
+print(mode)
