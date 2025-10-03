a0b670df2a4bb210819f2b6c08115e36bb310b00
umer <umery1730@gmail.com>
2025-10-03 18:45:42 +0500

    add

--- mult_str.py
+++ mult_str.py
@@ -1,7 +1,11 @@
 class Solution:
-    def multiply(self, num1: str, num2: str) -> str:
+    def multiply(self, num1: str, num2: str):
         n3=int(num1)
         n4=int(num2)
         n5=n3*n4
         n6=str(n5)
         return(n6)
+    
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.multiply("123", "456"))
